cb5db146921ca8fe40778abda93908260378cbca
shimanamisan <mikan.sup.all@gmail.com>
2023-10-19 08:04:10 +0900

    update: コメントを追加

--- public/RemoteProcedureCall/index.js
+++ public/RemoteProcedureCall/index.js
@@ -4,7 +4,9 @@ import Prompt from './Prompt/prompt.js';
 const client = new Client();
 const prompt = new Prompt();
 
+// メソッドを選択するプロンプトを実行する
 await prompt.runPromptSelectMethod();
+// 引数を選択するプロンプトを実行する
 await prompt.runPromptSelectArgument();
 
 const message = prompt.sendServerJsonMessage();
